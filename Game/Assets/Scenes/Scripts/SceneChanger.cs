af8db4bcf40b7dfdb8712e330d39c0507ed5daec
liamspiderhack <axelssonliam00@gmail.com>
2026-05-16 14:59:39 +0200

    Fixed button scene change

--- Game/Assets/Scenes/Scripts/SceneChanger.cs
+++ Game/Assets/Scenes/Scripts/SceneChanger.cs
@@ -1,21 +1,18 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneChanger : MonoBehaviour
+public class uiManager : MonoBehaviour
 {
 
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
     }
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
+
+
+
+
 }
